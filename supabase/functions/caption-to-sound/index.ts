465e193cc0724bd995a3a2883d957d75ed1b2579
Michael Lei <michaelleie0ca@gmail.com>
2023-10-29 23:41:08 -0700

    Finish creating edge function caption-to-sound

---
+++ supabase/functions/caption-to-sound/index.ts
@@ -0,0 +1,28 @@
+import Replicate from "https://esm.sh/replicate@^0.20.1";
+
+const replicate = new Replicate({
+  auth: Deno.env.get("REPLICATE_API_TOKEN"),
+});
+
+const model =
+  "sepal/audiogen:154b3e5141493cb1b8cec976d9aa90f2b691137e39ad906d2421b74c2a8c52b8";
+
+Deno.serve(async (req) => {
+  const { caption } = await req.json();
+
+  const input = {
+    prompt: caption,
+    duration: 3,
+    top_k: 250,
+    top_p: 0,
+    temperature: 1,
+    classifier_free_guidance: 3,
+    output_format: "mp3",
+  };
+
+  const output = await replicate.run(model, { input });
+
+  return new Response(JSON.stringify({ output }), {
+    headers: { "Content-Type": "application/json" },
+  });
+});
